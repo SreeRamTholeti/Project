571320c5e506894600eba5376c92e126e65a309d
SreeRamTholeti <48622637+SreeRamTholeti@users.noreply.github.com>
2021-07-06 14:13:55 +0530

    Updated xpath

--- Practice/Exampl_1.java
+++ Practice/Exampl_1.java
@@ -11,13 +11,11 @@ public class Exampl_1 {
 		Opensite os = new Opensite();
 		WebDriver driver = os.Open();
 		WebDriverWait wait = new WebDriverWait(driver, 20);
-		WebElement searchIcon = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div/div[2]/div[1]/p[2]/a"));
-		searchIcon.click();
-		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/div/div/div[2]/div[2]/div/a[1]")));
-		element.click();
-		WebElement message = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div[2]/form/div/input"));
-		message.sendKeys("My 1st Example");
-		WebElement submit = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div[2]/form/button"));
-		submit.click();
+		WebElement Form_demo = driver.findElement(By.xpath("//*[@id=\"basic\"]/div/a[1]"));
+		Form_demo.click();
+		WebElement input = driver.findElement(By.xpath("//input[@id=\"user-message\"]"));
+		input.sendKeys("My 1st Example");
+		WebElement show_msg = driver.findElement(By.xpath("//form[@id=\"get-input\"]//button[@class=\"btn btn-default\"]"));
+		show_msg.click();
 	}
 }
