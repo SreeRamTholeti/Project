ce7dac08da6264110b3c0c31156c4b0487cd8f24
SreeRamTholeti <48622637+SreeRamTholeti@users.noreply.github.com>
2021-07-06 13:59:20 +0530

    Update Opensite.java
    modified xpath declaration

--- Practice/Opensite.java
+++ Practice/Opensite.java
@@ -8,15 +8,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Opensite {
-	public WebDriver Open() {
+	public WebDriver Open(){
 		System.setProperty("webdriver.chrome.driver", "D:\\WorkSpace\\Day7\\src\\Webdrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 20);
-		driver.get("https://www.seleniumeasy.com/test/");
-		wait = new WebDriverWait(driver, 30);
-		WebElement popup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div/div[1]/div/div[2]/a")));
+		driver.get("https://www.seleniumeasy.com/");
+		WebElement Demo_site = driver.findElement(By.xpath("//div[@class=\"header-right\"]//a[@class=\"btn btn-success\"]"));
+		Demo_site.click();
+		WebElement popup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id=\"at-cv-lightbox-close\"]")));
 		popup.click();
-		WebElement Startpractice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/div/div/div[2]/div[1]/p[2]/a/span")));
+		WebElement Startpractice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id=\"btn_basic_example\"]")));
 		Startpractice.click();
 		return driver;
 	}
