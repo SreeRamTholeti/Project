985426805316a2416d315c41aa35adeec5ed337f
SreeRamTholeti <48622637+SreeRamTholeti@users.noreply.github.com>
2021-07-06 14:43:19 +0530

    Update Example_4.java

--- Practice/Example_4.java
+++ Practice/Example_4.java
@@ -12,16 +12,12 @@ public class Example_4 {
 		Opensite os = new Opensite();
 		WebDriver driver = os.Open();
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		
-		WebElement Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/div/div/div[2]/div[2]/div/a[2]")));
+		WebElement Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"basic\"]/div/a[2]")));
 		Checkbox.click();
 		List<WebElement> boxs = driver.findElements(By.className("cb1-element"));
 		int number_of_box = boxs.size(),i;
-		for(i=0;i<= number_of_box;i++) {
+		for(i=0;i<number_of_box;i++) {
 			boxs.get(i).click();
-			wait = new WebDriverWait(driver,10);
 		}
-//		WebElement checkall = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/input[1]")));
-//		checkall.click();
 	}
 }
