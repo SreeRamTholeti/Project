80b9b5d59c6cec0ee7e59a56ca7b95ffed88e2e9
SreeRamTholeti <48622637+SreeRamTholeti@users.noreply.github.com>
2021-07-13 17:33:46 +0530

    Update Getfromxlsx.java

--- day8/Getfromxlsx.java
+++ day8/Getfromxlsx.java
@@ -1,7 +1,6 @@
 package Task_8;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 import org.apache.poi.ss.usermodel.CellType;
@@ -10,8 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Getfromxlsx{
-	static File file = new File("D:\\WorkSpace\\Day8\\src\\Task_8\\Names.xlsx");
-	//Geting number of rows
+	File file = new File("D:\\WorkSpace\\Day8\\src\\Task_8\\Names.xlsx");
 	public int get_rowcount() throws Exception {
 		FileInputStream fis = new FileInputStream(file);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
@@ -20,54 +18,36 @@ public class Getfromxlsx{
 		workbook.close();
 		return(row_count);
 	}
-	//geting cellvalues
-	public static String get_Cellvalue(int row_num,int col_num) throws Exception {
-		String cell_value = "";
+	public void savingtoxlsx(String set[],int size) throws Exception {
 		FileInputStream fis = new FileInputStream(file);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		XSSFSheet sheet = workbook.getSheet("Sheet1");
-		XSSFRow row = sheet.getRow(row_num);
-		CellType cell_type = row.getCell(col_num).getCellType();
-		switch(cell_type) {
-		case STRING:cell_value = row.getCell(col_num).getStringCellValue(); break;
-		case NUMERIC:
-			Integer value = (int)row.getCell(col_num).getNumericCellValue();
-			cell_value =  value.toString();
-			break;		
-		}
-		return (cell_value);
-	}
-	//Creating new sheet with headings
-	void Create_newSheet() throws Exception{
-		FileInputStream fis = new FileInputStream(file);
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		XSSFSheet sheet = workbook.createSheet("New_Sheet");
-		sheet.createRow(0);
-		sheet.getRow(0).createCell(0).setCellValue("Number");
-		sheet.getRow(0).createCell(1).setCellValue("Serial No.");
-		sheet.getRow(0).createCell(2).setCellValue("Name");
-		FileOutputStream fos = new FileOutputStream(file);
-		workbook.write(fos);
-		workbook.close();
-		fis.close();
-		fos.close();
-	}
-	//setting values to new sheet
-	void save_to_sheet(String set[],int size) throws Exception {
-		FileInputStream fis = new FileInputStream(file);
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		XSSFSheet sheet_2 = workbook.getSheet("New_Sheet");
-		int i,row_num;
-		XSSFRow row;
-		String value_2,value_1;
+		XSSFSheet sheet_0 = workbook.getSheetAt(0);
+		XSSFSheet sheet_1 = workbook.createSheet("My_newsheet");
+		String heading []={"Number","serial_No.","Name"};
+		int i,j,k,row_num,col_num,h_size=heading.length;
+		XSSFRow row,row_0;
+		CellType cell_type;
 		for(i=0;i<size;i++) {
-			row = sheet_2.createRow(i+1);
+			
 			row_num = Integer.valueOf(set[i]);
-			value_1 = get_Cellvalue(row_num,0);
-			value_2 = get_Cellvalue(row_num,1);
-			row.createCell(0).setCellValue(set[i]);
-			row.createCell(1).setCellValue(value_1);
-			row.createCell(2).setCellValue(value_2);
+			row_0 = sheet_0.getRow(row_num);
+			col_num = row_0.getLastCellNum();
+			if(i == 0) {
+				row = sheet_1.createRow(i); 
+				for(k=0;k<h_size;k++) {
+					row.createCell(k).setCellValue(heading[k]);
+				}	
+			}
+			row = sheet_1.createRow(i+1);
+			for(j=0;j<col_num;j++) {
+				row.createCell(0).setCellValue(set[i]);
+				cell_type = row_0.getCell(j).getCellType();
+				switch(cell_type) {
+				case STRING:row.createCell(j+1).setCellValue(row_0.getCell(j).getStringCellValue()); break;
+				case NUMERIC:row.createCell(j+1).setCellValue(row_0.getCell(j).getNumericCellValue());break;
+				default :row.createCell(j+1).setCellValue("");break;
+				}
+			}
 		}
 		FileOutputStream fos = new FileOutputStream(file);
 		workbook.write(fos);
