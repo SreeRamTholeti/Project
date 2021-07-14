bf7a30a90cacd14f2bbad3f3ae89cbac32225f8f
SreeRamTholeti <48622637+SreeRamTholeti@users.noreply.github.com>
2021-07-14 20:38:30 +0530

    Robot class is used to take screenshot

--- Day_9/takeScreenshot.java
+++ Day_9/takeScreenshot.java
@@ -1,7 +1,13 @@
 package Task9;
 
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,9 +19,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
 public class takeScreenshot {
-	File clickpic() throws Exception{
+	String clickpic() throws Exception{
 		System.setProperty("webdriver.chrome.driver","D:\\WorkSpace\\Day8\\src\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.random.org/");
 		Thread.sleep(500);
 		List <WebElement> links = driver.findElements(By.xpath("//div[@class]//a"));
@@ -36,14 +43,17 @@ public class takeScreenshot {
 		WebElement set = driver.findElement(By.xpath("//ul[@class=\"data\"]//li"));
 		
 		JavascriptExecutor js = (JavascriptExecutor)(driver);
-		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');",set);
+		js.executeScript("arguments[0].setAttribute('style', 'border: 2px solid red;');",set);
 		Thread.sleep(1000);
+		String current_url = driver.getCurrentUrl();
 		
-		TakesScreenshot tc = (TakesScreenshot)(driver);
-		File pic = tc.getScreenshotAs(OutputType.FILE);
-		File image = new File(".//random.jpeg");
-		FileHandler.copy(pic,image);
-		return(image);
+		Robot rob = new Robot();
+		Rectangle screenSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		BufferedImage img_robot = rob.createScreenCapture(screenSize);
+		String path=System.getProperty("user.dir")+"RImage.jpeg";
+		ImageIO.write(img_robot, "jpeg",new File(path)); 
+        return path;
+		
 	}
 
 }
